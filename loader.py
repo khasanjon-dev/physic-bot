d1d238a1ed1ba4f3eb12f3e716d1aa30edbaaf9d
Khasanjon Mamadaliyev <khasanjon.eng@gmail.com>
2023-06-21 11:01:28 +0500

    add .env

--- loader.py
+++ loader.py
@@ -1,8 +1,10 @@
 import logging
-
+import os
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 
-API_TOKEN = '5927474751:AAF53TaZ4VtxIB5wlIfHD5l-YZpbziw2DJ4'
+load_dotenv()
+API_TOKEN = os.getenv('API_TOKEN')
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
