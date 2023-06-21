b5ce9ba6d5d8c599a818436fb2e8b36364d45312
Khasanjon Mamadaliyev <khasanjon.eng@gmail.com>
2023-06-21 11:39:56 +0500

    add send photo

--- handlers.py
+++ handlers.py
@@ -1,11 +1,12 @@
 import os
 
+from aiogram import types
 from aiogram.dispatcher.filters import CommandStart, CommandHelp
 from aiogram.types import Message
 
 from loader import dp, bot
 
-ADMIN_ID = os.getenv('admin_id')
+ADMIN_IDS = os.getenv('ADMIN_ID').split(',')
 
 
 @dp.message_handler(CommandStart())
@@ -17,17 +18,26 @@ async def send_welcome(message: Message):
 
 
 @dp.message_handler(CommandHelp())
-async def help_message(message: Message):
+async def help_message(msg: Message):
     text = "Savolingizni yozing va biz sizga imkon qadar tez orada javob beramiz."
-    await message.answer(text)
+    await msg.answer(text)
 
 
-
-@dp.message_handler(content_types=)
+@dp.message_handler(content_types=types.ContentType.PHOTO)
+async def send_photo(msg: Message):
+    photo_file_id = msg.photo[-1].file_id
+    text = f"Xabar {msg.from_user.first_name} dan yuborildi !\n" \
+           f"@{msg.from_user.username}"
+    text += f"\n\n{msg.caption}"
+    for admin_id in ADMIN_IDS:
+        await bot.send_photo(admin_id, photo=photo_file_id, caption=text)
+    await msg.answer('rasm adminga yuborildi 🙂')
 
 
 @dp.message_handler()
 async def response(msg: Message):
-    text = "Savol adminga yuborildi 😊"
-    bot.send_message(ADMIN_ID, msg.text)
-    await msg.reply(text)
+    text = f"{msg.from_user.first_name} dan\n\n" \
+           f"{msg.text}"
+    for admin_id in ADMIN_IDS:
+        await bot.send_message(admin_id, text)
+    await msg.answer('xabar adminga yuborildi 😊')
--- loader.py
+++ loader.py
@@ -1,5 +1,7 @@
 import logging
 import os
+
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 
@@ -7,4 +9,5 @@ load_dotenv()
 API_TOKEN = os.getenv('API_TOKEN')
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
+# storage = MemoryStorage()
 dp = Dispatcher(bot)
